788ad7179c08e3ce534f111d0457dc12eb606555
Kovács Zsolt <smith.zsolt@gmail.com>
2020-03-31 16:22:38 +0200

    remove console.log

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const fs = require('fs');
 const jsonData = require('./country.full.json');
 const minList=jsonData.map(i=> {return {code: i.code,native: i.native,name: i.name}}); 
-console.log(minList);
 fs.writeFileSync('./country.hu.json', JSON.stringify(minList));
